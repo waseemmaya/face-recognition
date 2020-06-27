e174ad618405fffec8d9cf977cbb44efd96ce2c9
Waseem Maya <waseemmayaa@gmail.com>
2020-06-27 23:31:01 +0500

    added my custom images

--- script.js
+++ script.js
@@ -56,9 +56,9 @@ function loadLabeledImages() {
     labels.map(async (label) => {
       const descriptions = [];
       for (let i = 1; i <= 2; i++) {
-        const img = await faceapi.fetchImage(
-          `https://raw.githubusercontent.com/WebDevSimplified/Face-Recognition-JavaScript/master/labeled_images/${label}/${i}.jpg`
-        );
+        let url = `https://raw.githubusercontent.com/waseemmaya/face-recognition/master/labeled_images/${label}/${i}.jpg`;
+
+        const img = await faceapi.fetchImage(url);
         const detections = await faceapi
           .detectSingleFace(img)
           .withFaceLandmarks()
